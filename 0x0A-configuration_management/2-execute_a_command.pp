c956fa03edb137026dd38fbab4a6059137b63180
Omar Elsendiony <omar.sendiony@gmail.com>
2024-04-13 15:35:03 +0200

    ok

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# kill process killmenow
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
+exec { 'killmenow':
+    command => 'pkill -f killmenow',
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+    onlyif  => 'pgrep -f killmenow',
 }
