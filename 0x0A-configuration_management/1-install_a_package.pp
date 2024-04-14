2722bc0f90f4ca55cd98b4b422f3480cfd778217
Omar Elsendiony <omar.sendiony@gmail.com>
2024-04-14 02:46:59 +0200

    try

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
 # Install an especific version of flask (2.1.0)
+package {'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+}
+
 package {'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
